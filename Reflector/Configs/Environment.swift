31171c42bed8522af18f7a64da2e82f92cc265c0
Derrick Rocha <drocha616@gmail.com>
2019-09-14 22:32:29 -0600

    Add ad mob (#2)
    * Added Admob dependency.
    
    * Added admob Id.
    
    * Added paid on free versions. Disabling Admob for paid version.

---
+++ Reflector/Configs/Environment.swift
@@ -0,0 +1,49 @@
+//
+//  Environment.swift
+//  Reflector
+//
+//  Created by derrick rocha on 9/14/19.
+//  Copyright © 2019 derrick rocha. All rights reserved.
+//
+
+import Foundation
+
+public enum Environment{
+    
+    // MARK: - Keys
+    enum Keys {
+        enum Plist {
+            static let admobUnitId = "AdMobUnitId"
+            static let type = "appType"
+        }
+    }
+    
+    // MARK: - Plist
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist file not found")
+        }
+        return dict
+    }()
+
+    // MARK: - Plist values
+    static let adMobUnitId: String = {
+        guard let id = Environment.infoDictionary[Keys.Plist.admobUnitId] as? String else{
+            fatalError("Admob unit id not set for this environment")
+        }
+        return id
+    }()
+    
+    // MARK: - Plist values
+    static let isPaid: Bool = {
+        guard let type = Environment.infoDictionary[Keys.Plist.type] as? String else{
+                fatalError("App type not set for this enironment")
+        }
+        if(type == "free"){
+            return false
+        }
+        else{
+            return true
+        }
+    }()
+}
